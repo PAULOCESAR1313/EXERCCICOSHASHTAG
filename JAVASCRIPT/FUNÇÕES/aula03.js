bee56f83b53859f3fa5083d6605a344eab6e0af3
Paulo César <soci4lmidi4@gmail.com>
2025-05-02 23:41:36 -0300

    AULA 03 CHAMADANDO FUNÇÃO
    Aprendemos a fazer uma chamada de função da forma correta

---
+++ JAVASCRIPT/FUNÇÕES/aula03.js
@@ -0,0 +1,8 @@
+//Como chamar uma função da maneira correta.
+
+function exibirDetalhes(nome, curso)
+{
+    console.log(`O professor ${nome}, que é responsavel pelo ${curso}, é formado em Analise e Desenvolvimento de Softwere`);
+};
+
+exibirDetalhes("Paulo César", "Javascript imprecionador");
