89fe51997249944c4475dc565c4d26fe4cdf71a9
Paulo César <soci4lmidi4@gmail.com>
2025-05-12 13:50:24 -0300

    Create exer01e02.js

---
+++ JAVASCRIPT/METODOSNATIVOSOBJTOS/OBJETOS GLOBAS/EXERCICIO/exer01e02.js
@@ -0,0 +1,3 @@
+//EXERCICIO - 01
+//Você precisa criar uma função que receba um número decimal e o arredonde de três formas diferente: Para baixo, para cima e para o inteiro mais proximo. Mostre cada arredondamento no console.
+//EXERCICIO - 02
