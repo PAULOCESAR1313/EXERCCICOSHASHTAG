8302b0d58b5581f28e2a703b426f7274ec5ef409
Paulo César <soci4lmidi4@gmail.com>
2025-05-16 23:15:47 -0300

    Update exercicio003.js

--- JAVASCRIPT/METODOSNATIVOSOBJTOS/ESTRUTURA DE REPETIÇÃO/EXERCICIOS/exercicio003.js
+++ JAVASCRIPT/METODOSNATIVOSOBJTOS/ESTRUTURA DE REPETIÇÃO/EXERCICIOS/exercicio003.js
@@ -1,1 +1,32 @@
 //EXERCICIO - 003
+/*Você esta gerenciando uma equipe de vendas, e deseja avaliar o desempenho dos seus vendedores, com base em suas vendas mensais em relação a uma meta estabelecida. Escreva um programa desempenhoIndividualDoVendedor que classifique em categorias de desempenho dos vendedores com base no percentual alcançado.*/
+
+const vendedor = 
+[
+    {
+        nome: "Paulo",
+        vendas: 1000,
+    }
+
+    {
+        nome: "Joaquim",
+        vendas: 13000,
+    }
+
+    {
+        nome: "Thayanne",
+        vendas: 12000,
+    }
+
+    {
+        nome: "Thanara",
+        vendas: 7000,
+    }
+
+    {
+        nome: "Maria",
+        vendas: 3000,
+    }
+];
+
+console.log(`${vendedor[0]}`);
