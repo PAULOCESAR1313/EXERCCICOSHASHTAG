9e77ac77f6cc6316d9e2ebda51b1de4474b895f5
Paulo César <soci4lmidi4@gmail.com>
2025-05-25 10:24:06 -0300

    INCOMPLETO

---
+++ JAVASCRIPT/METODOSNATIVOSOBJTOS/ESTRUTURA DE REPETIÇÃO/EXERCICIOS/exercicio004.js
@@ -0,0 +1,7 @@
+//Criando um contador dias trabalhado - diasTrabalhados.
+/*Condição o nome do funcionario tem que ser passado se não deve aparecer uma mensagem informando que falta o nome do funcionario,*/
+
+for(let index = 0; index <= 31; index++ )
+    {
+        
+    };
