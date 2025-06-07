64bc67680c0ae8f54dc915eb26054f52d01712f0
Paulo César <soci4lmidi4@gmail.com>
2025-06-07 07:40:37 -0300

    AULA 42
    Evento onclick e função para capturar o conteúdo do campo texto.

---
+++ JAVASCRIPT/FORMULARIO/AULA42/aula42.js
@@ -0,0 +1,7 @@
+function mostrarValorCampo()
+{
+    let valorCampo = document.getElementById("campoTexto").value; //armazena o valor do campo texto em uma variavel.
+
+    //Exibindo o valor do campo texto atraves do alert
+    alert(`O valor do campo de texto é: ${valorCampo}`);
+};
